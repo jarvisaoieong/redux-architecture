f3d16f0137b51dfb8da6c835ccf2798c0db66fd9
Jarvis Ao Ieong <kinua1230@gmail.com>
2016-03-06 12:01:03 +0800

    update hot reload setting

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import createStore from './helpers/createStore';
-import {App, reducer, init} from 'modules/app';
+import {reducer, init} from 'modules/app';
 
 const store = createStore(reducer, init());
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>
-,
-  document.getElementById('app')
-);
+const render = () => {
+  const {App} = require('modules/app');
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ,
+    document.getElementById('app')
+  );
+}
+
+render();
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
   module.hot.accept('modules/app', () => {
     const {reducer: nextReducer} = require('modules/app');
     store.replaceReducer(nextReducer);
+    render();
   });
 }
--- src/modules/app/components/App.js
+++ src/modules/app/components/App.js
@@ -8,32 +8,26 @@ import RandomGifContainer from './RandomGifContainer';
 import RandomGifPairContainer from './RandomGifPairContainer';
 import RandomGifListContainer from './RandomGifListContainer';
 
-export default class App extends Component {
-
-  render() {
-    return (
-      <div>
-        <h2>1. Counter</h2>
-        <CounterContainer />
-        <hr/>
-        <h2>2. CounterPair</h2>
-        <CounterPairContainer />
-        <hr/>
-        <h2>3. CounterList</h2>
-        <CounterListContainer />
-        <hr/>
-        <h2>4. CounterFancy</h2>
-        <CounterFancyContainer />
-        <hr/>
-        <h2>5. RandomGif</h2>
-        <RandomGifContainer />
-        <hr/>
-        <h2>6. RandomGifPair</h2>
-        <RandomGifPairContainer />
-        <hr/>
-        <h2>7. RandomGifList</h2>
-        <RandomGifListContainer />
-      </div>
-    );
-  }
-}
+export default () =>
+  <div>
+    <h2>1. Counter</h2>
+    <CounterContainer />
+    <hr/>
+    <h2>2. CounterPair</h2>
+    <CounterPairContainer />
+    <hr/>
+    <h2>3. CounterList</h2>
+    <CounterListContainer />
+    <hr/>
+    <h2>4. CounterFancy</h2>
+    <CounterFancyContainer />
+    <hr/>
+    <h2>5. RandomGif</h2>
+    <RandomGifContainer />
+    <hr/>
+    <h2>6. RandomGifPair</h2>
+    <RandomGifPairContainer />
+    <hr/>
+    <h2>7. RandomGifList</h2>
+    <RandomGifListContainer />
+  </div>
